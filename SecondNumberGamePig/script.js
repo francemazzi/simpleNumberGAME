8d8e66d9fa29bb8fe3a8d7b10b7a05a899573d16
fraSma <96951898+francemazzi@users.noreply.github.com>
2022-07-16 11:11:09 +0200

    Add files via upload

---
+++ SecondNumberGamePig/script.js
@@ -0,0 +1,111 @@
+'use strict';
+
+//SELECTING ELEMENTS
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnNewRoll = document.querySelector('.btn--roll');
+const btnNewHold = document.querySelector('.btn--hold');
+
+//Dichiariamo le variabili nelle funzioni 'scoping'
+let score, currentScore, activePlayer, playing;
+
+//STARTING CONDITIONS
+
+const init = function () {
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+//Per fare correre la funzione iniziale basta farla correre chiamandola:
+init();
+
+const switchPlayer = function () {
+  document.getElementById('current--' + activePlayer).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+//ROLLING DICE FUNCTIONALITY
+btnNewRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceEl.classList.remove('hidden');
+    diceEl.src = 'dice-' + dice + '.png';
+
+    if (dice !== 1) {
+      currentScore = currentScore + dice;
+      document.getElementById('current--' + activePlayer).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNewHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Current score to score active player
+    score[activePlayer] += currentScore;
+    document.getElementById('score--' + activePlayer).textContent =
+      score[activePlayer];
+
+    //2. Controllare che punteggio sia 100
+    if (score[activePlayer] >= 10) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector('.player--' + activePlayer)
+        .classList.add('player--winner');
+      document
+        .querySelector('.player--' + activePlayer)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer(activePlayer);
+    }
+  }
+});
+
+//Reset the game
+// btnNew.addEventListener('click', function () {
+//   document
+//     .querySelector('.player--' + activePlayer)
+//     .classList.add('player--active');
+//   document.getElementById('current--0').textContent = 0;
+//   document.getElementById('current--1').textContent = 0;
+//   document.getElementById('score--0').textContent = 0;
+//   document.getElementById('score--1').textContent = 0;
+//   diceEl.classList.remove('hidden');
+//   if (score[activePlayer] >= 10) {
+//     document
+//       .querySelector('.player--' + activePlayer)
+//       .classList.remove('player--winner');
+//   }
+// });
+
+// Dobbiamo dichiarare una ariabiile anonomia, non dobbiamo chiamare la funzione quindi sarà js a farlo!
+btnNew.addEventListener('click', init);
